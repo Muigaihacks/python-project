44e476e946c2a56c2b647fcd3b8279a2a1c30cbb
Muigaihacks <146855637+Muigaihacks@users.noreply.github.com>
2023-10-03 17:52:11 +0300

    Initial commit

---
+++ assignment1.py
@@ -0,0 +1,8 @@
+# Tyrese Muigai SCT211-0457/2022
+
+num1= input("Enter the first digit: ")
+num2= input("Enter the second digit: ")
+
+sum = num1 + num2
+
+print("The sum of " +num1+ " and "+num2+" is "+ sum)
